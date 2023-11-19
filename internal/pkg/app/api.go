e275fdf121dbc6c91708a81b2ec0d15581095a80
frosnik <frosnikny@yandex.ru>
2023-11-19 20:00:08 +0300

    Update api.go

--- internal/pkg/app/api.go
+++ internal/pkg/app/api.go
@@ -14,14 +14,14 @@ func (a *Application) GetDevelopmentService(c *gin.Context) {
 		return
 	}
 
-	container, err := a.repo.GetDevelopmentServiceByID(request.DevelopmentServiceId)
+	developmentService, err := a.repo.GetDevelopmentServiceByID(request.DevelopmentServiceId)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	if container == nil {
+	if developmentService == nil {
 		c.AbortWithError(http.StatusNotFound, fmt.Errorf("услуга по разработке не найдена"))
 		return
 	}
-	c.JSON(http.StatusOK, container)
+	c.JSON(http.StatusOK, developmentService)
 }
